28072a6078f8a6ce408dd083cc1f27d225af7b5c
Konrad Makowski <41269364+SpicyHolo@users.noreply.github.com>
2023-11-17 09:54:01 +0100

    Fix not connecting spw, clk50 signals, change pin assignments

--- spacewire_top.vhd
+++ spacewire_top.vhd
@@ -1,37 +1,41 @@
 --
---  Test of spwstream on Digilent XC3S200 board.
---  60 MHz system clock, 200 MHz receive clock and transmit clock.
+--  Test of spwstream on Pender GR-XC3S-1500 board.
+--  60 MHz system clock; 200 MHz receive clock and transmit clock.
 --
---  LED 0 = link started
---  LED 1 = link connecting
---  LED 2 = link run
---  LED 3 = link error (sticky until clear button)
---  LED 4 = gotdata
---  LED 5 = off
---  LED 6 = data error (sticky until reset)
---  LED 7 = time code error (sticky until reset)
+--  LED 0 = link run
+--  LED 1 = link error (sticky until clear button)
+--  LED 2 = gotdata
+--  LED 3 = data/timecode error (sticky until reset)
 --
---  Button 0 = reset
---  Button 1 = clear LED 3
+--  Button S2 = reset
+--  Button S3 = clear LED 1
 --
---  Switch 0 = link autostart
---  Switch 1 = link start
---  Switch 2 = link disable
---  Switch 3 = send data and time codes
---  Switch 4-7 = bits 0-3 of tx bit rate scale factor x
+--  Switch 0 = link start
+--  Switch 1 = link disable
+--  Switch 2 = send data
+--  Switch 3 = send time codes
+--  Switch 4-7 = bits 0-3 of tx bit rate scale factor
 --
---  SpaceWire signals on A2 expansion connector:
---    Data In    pos,neg  =  B5,C5  =  pin 19,6
---    Strobe In  pos,neg  =  D6,E6  =  pin 7,4
---    Data Out   pos,neg  =  B6,C6  =  pin 21,8
---    Strobe Out pos,neg  =  D7,E7  =  pin 11,9
+--  SpaceWire signals on expansion connector J12:
+--    Data In    pos,neg  =  m1,m2  =  pin 3,2
+--    Strobe In  pos,neg  =  m3,m4  =  pin 6,5
+--    Data Out   pos,neg  =  n1,n2  =  pin 9,8
+--    Strobe Out pos,neg  =  n3,n4  =  pin 12,11
 --
---  Note: these are not true LVDS signals; they are configured as LVDS25
---  but powered from 3.3V instead of 2.5V, not differentially routed and
---  not properly terminated.
+--  To get proper LVDS signals from connector J12, the voltage on I/O bank 6
+--  must be set to 2.5V. This is the default on GR-XC3S-1500-rev2, but on
+--  GR-XC3S-1500-rev1 a change is required on the board (described in
+--  the board manual).
 --
---  The SpaceWire port should be looped back to itself with wires from
---  outputs to corresponding inputs.
+--  To terminate the incoming LVDS signals, 100 Ohm termination resistors
+--  must be installed on the board in positions R120 and R121.
+--
+--  The SpaceWire port should be looped back to itself, either directly
+--  or via an other SpaceWire device. For a direct loopback, place 4 wires
+--  from the output pins to the corresponding input pins. For an indirect
+--  loopback, connect the SpaceWire signals to an additional SpaceWire device
+--  which is programmed to echo everything it receives (characters, packets,
+--  time codes). See the datasheet for a wiring diagram from J12 to MDM9.
 --
 
 library ieee;
@@ -41,21 +45,21 @@ use work.spwpkg.all;
 entity streamtest_top is
 
     port (
-        clk50:    in  std_logic;
-        button:   in  std_logic_vector(3 downto 0);
-        switch:   in  std_logic_vector(7 downto 0);
-        led:      out std_logic_vector(7 downto 0);
-        spw_di:   in  std_logic;
-        spw_si:   in  std_logic;
-        spw_do:   out std_logic;
-        spw_so:   out std_logic;
+        clk50:        in  std_logic;
+        btn_reset:  in  std_logic;
+        btn_clear:  in  std_logic;
+        switch:     in  std_logic_vector(3 downto 0);
+        led:        out std_logic_vector(3 downto 0);
+        spw_di:     in  std_logic;
+        spw_si:     in  std_logic;
+        spw_do:     out std_logic;
+        spw_so:     out std_logic );
 
 end entity streamtest_top;
 
 architecture streamtest_top_arch of streamtest_top is
 
     -- Clock generation.
-    signal boardclk:        std_logic;
     signal sysclk:          std_logic;
 
     -- Synchronize buttons
@@ -71,6 +75,7 @@ architecture streamtest_top_arch of streamtest_top is
     signal s_autostart:     std_logic := '0';
     signal s_linkdisable:   std_logic := '0';
     signal s_senddata:      std_logic := '0';
+    signal s_sendtick:      std_logic := '0';
     signal s_txdivcnt:      std_logic_vector(7 downto 0) := "00000000";
     signal s_linkstarted:   std_logic;
     signal s_linkconnecting: std_logic;
@@ -83,6 +88,7 @@ architecture streamtest_top_arch of streamtest_top is
     signal s_spwsi:         std_logic;
     signal s_spwdo:         std_logic;
     signal s_spwso:         std_logic;
+
 
     component streamtest is
         generic (
@@ -122,24 +128,24 @@ begin
     -- Streamtest instance
     streamtest_inst: streamtest
         generic map (
-            sysfreq     => 60.0e6,
+            sysfreq     => 50.0e6,
             txclkfreq   => 0.0,
-            tickdiv     => 20,
+            tickdiv     => 22,
             rximpl      => impl_generic,
             rxchunk     => 1,
             tximpl      => impl_generic,
             rxfifosize_bits => 11,
-            txfifosize_bits => 11 )
+            txfifosize_bits => 10 )
         port map (
             clk         => sysclk,
-            rxclk       => "0", -- Only used with impl_fast
-            txclk       => "0", -- Only used with impl_fast
+            rxclk       => '0',
+            txclk       => '0',
             rst         => s_rst,
             linkstart   => s_linkstart,
             autostart   => s_autostart,
             linkdisable => s_linkdisable,
             senddata    => s_senddata,
-            sendtick    => s_senddata,
+            sendtick    => s_sendtick,
             txdivcnt    => s_txdivcnt,
             linkstarted => s_linkstarted,
             linkconnecting => s_linkconnecting,
@@ -152,37 +158,40 @@ begin
             spw_si      => s_spwsi,
             spw_do      => s_spwdo,
             spw_so      => s_spwso );
+				
+	 sysclk <= clk50;
+	 s_spwdi <= spw_di;
+    s_spwsi <= spw_si;
+    spw_do <= s_spwdo;
+    spw_so <= s_spwso;
 
     process (sysclk) is
     begin
         if rising_edge(sysclk) then
 
             -- Synchronize buttons
-            s_resetbtn  <= not button(0);
+            s_resetbtn  <= not btn_reset;
             s_rst       <= s_resetbtn;
-            s_clearbtn  <= not button(1);
+            s_clearbtn  <= not btn_clear;
 
             -- Synchronize switch settings
-            s_autostart <= switch(0);
-            s_linkstart <= switch(1);
-            s_linkdisable <= switch(2);
-            s_senddata  <= switch(3);
-            s_txdivcnt(7 downto 0) <= (others => "0");
+            s_autostart <= '0';
+            s_linkstart <= switch(0);
+            s_linkdisable <= switch(1);
+            s_senddata  <= switch(2);
+            s_sendtick  <= switch(3);
+            s_txdivcnt(7 downto 0) <= "00000000";
 
             -- Sticky link error LED
             s_linkerrorled <= (s_linkerrorled or s_linkerror) and
                               (not s_clearbtn) and
                               (not s_resetbtn);
 
-            -- Drive LEDs
-            led(0)  <= s_linkstarted;
-            led(1)  <= s_linkconnecting;
-            led(2)  <= s_linkrun;
-            led(3)  <= s_linkerrorled;
-            led(4)  <= s_gotdata;
-            led(5)  <= '0';
-            led(6)  <= s_dataerror;
-            led(7)  <= s_tickerror;
+            -- Drive LEDs (inverted logic)
+            led(0)  <= s_linkrun;
+            led(1)  <= s_linkerrorled;
+            led(2)  <= s_gotdata;
+            led(3)  <= (s_dataerror or s_tickerror);
 
         end if;
     end process;
