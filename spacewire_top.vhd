e93a389abc41158e2a357cbb2ca1684b02386126
Konrad Makowski <41269364+SpicyHolo@users.noreply.github.com>
2023-12-09 12:55:21 +0100

    Revert to working streamtest with updated LVDS pin locations.

--- spacewire_top.vhd
+++ spacewire_top.vhd
@@ -18,7 +18,7 @@
 --  Switch 1 = link start
 --  Switch 2 = link disable
 --  Switch 3 = send data and time codes
---  Switch 4-7 = bits 0-3 of tx bit rate scale factor x
+--  Switch 4-7 = bits 0-3 of tx bit rate scale factor
 --
 --  SpaceWire signals on A2 expansion connector:
 --    Data In    pos,neg  =  B5,C5  =  pin 19,6
@@ -34,6 +34,13 @@
 --  outputs to corresponding inputs.
 --
 
+--  The SpaceWire port should be looped back to itself, either directly
+--  or via an other SpaceWire device. For a direct loopback, place 4 wires
+--  from the output pins to the corresponding input pins. For an indirect
+--  loopback, connect the SpaceWire signals to an additional SpaceWire device
+--  which is programmed to echo everything it receives (characters, packets,
+--  time codes).
+
 library ieee;
 use ieee.std_logic_1164.all, ieee.numeric_std.all;
 use work.spwpkg.all;
@@ -41,21 +48,21 @@ use work.spwpkg.all;
 entity streamtest_top is
 
     port (
-        clk50:    in  std_logic;
-        button:   in  std_logic_vector(3 downto 0);
-        switch:   in  std_logic_vector(7 downto 0);
-        led:      out std_logic_vector(7 downto 0);
-        spw_di:   in  std_logic;
-        spw_si:   in  std_logic;
-        spw_do:   out std_logic;
-        spw_so:   out std_logic);
+        clk50:        in  std_logic;
+        btn_reset:  in  std_logic;
+        btn_clear:  in  std_logic;
+        switch:     in  std_logic_vector(3 downto 0);
+        led:        out std_logic_vector(3 downto 0);
+        spw_di:     in  std_logic;
+        spw_si:     in  std_logic;
+        spw_do:     out std_logic;
+        spw_so:     out std_logic );
 
 end entity streamtest_top;
 
 architecture streamtest_top_arch of streamtest_top is
 
     -- Clock generation.
-    signal boardclk:        std_logic;
     signal sysclk:          std_logic;
 
     -- Synchronize buttons
@@ -71,6 +78,7 @@ architecture streamtest_top_arch of streamtest_top is
     signal s_autostart:     std_logic := '0';
     signal s_linkdisable:   std_logic := '0';
     signal s_senddata:      std_logic := '0';
+    signal s_sendtick:      std_logic := '0';
     signal s_txdivcnt:      std_logic_vector(7 downto 0) := "00000000";
     signal s_linkstarted:   std_logic;
     signal s_linkconnecting: std_logic;
@@ -83,6 +91,7 @@ architecture streamtest_top_arch of streamtest_top is
     signal s_spwsi:         std_logic;
     signal s_spwdo:         std_logic;
     signal s_spwso:         std_logic;
+
 
     component streamtest is
         generic (
@@ -122,7 +131,7 @@ begin
     -- Streamtest instance
     streamtest_inst: streamtest
         generic map (
-            sysfreq     => 60.0e6,
+            sysfreq     => 50.0e6,
             txclkfreq   => 0.0,
             tickdiv     => 20,
             rximpl      => impl_generic,
@@ -132,14 +141,14 @@ begin
             txfifosize_bits => 11 )
         port map (
             clk         => sysclk,
-            rxclk       => "0", -- Only used with impl_fast
-            txclk       => "0", -- Only used with impl_fast
+            rxclk       => '0',
+            txclk       => '0',
             rst         => s_rst,
             linkstart   => s_linkstart,
             autostart   => s_autostart,
             linkdisable => s_linkdisable,
             senddata    => s_senddata,
-            sendtick    => s_senddata,
+            sendtick    => s_sendtick,
             txdivcnt    => s_txdivcnt,
             linkstarted => s_linkstarted,
             linkconnecting => s_linkconnecting,
@@ -152,29 +161,36 @@ begin
             spw_si      => s_spwsi,
             spw_do      => s_spwdo,
             spw_so      => s_spwso );
+				
+	 sysclk <= clk50;
+	 s_spwdi <= spw_di;
+    s_spwsi <= spw_si;
+    spw_do <= s_spwdo;
+    spw_so <= s_spwso;
 
     process (sysclk) is
     begin
         if rising_edge(sysclk) then
 
             -- Synchronize buttons
-            s_resetbtn  <= not button(0);
+            s_resetbtn  <= not btn_reset;
             s_rst       <= s_resetbtn;
-            s_clearbtn  <= not button(1);
+            s_clearbtn  <= not btn_clear;
 
             -- Synchronize switch settings
-            s_autostart <= switch(0);
-            s_linkstart <= switch(1);
-            s_linkdisable <= switch(2);
-            s_senddata  <= switch(3);
-            s_txdivcnt(7 downto 0) <= (others => "0");
+            s_autostart <= '0';
+            s_linkstart <= switch(0);
+            s_linkdisable <= switch(1);
+            s_senddata  <= switch(2);
+            s_sendtick  <= switch(3);
+            s_txdivcnt(7 downto 0) <= "00000000";
 
             -- Sticky link error LED
             s_linkerrorled <= (s_linkerrorled or s_linkerror) and
                               (not s_clearbtn) and
                               (not s_resetbtn);
 
-            -- Drive LEDs
+            -- Drive LEDs (inverted logic)
             led(0)  <= s_linkstarted;
             led(1)  <= s_linkconnecting;
             led(2)  <= s_linkrun;
